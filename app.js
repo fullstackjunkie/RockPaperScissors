a76414f16c25d99c35caa55a141d05e6f0035bba
fullstackjunkie <dirtroadddeveloper@gmail.com>
2024-10-23 15:57:38 -0400

    finished editing

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 
 
 
-
-
-
+const btn = document.querySelector("#btn");
+const results = document.querySelector(".results");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const score = document.createElement("p");
+const compSc = document.createElement("p");
+const p = document.createElement("p");
+results.appendChild(p);
+results.appendChild(score);
+results.appendChild(compSc);
 let humScore = 0;
 let compScore = 0;
 
@@ -26,9 +33,9 @@ function getComputerChoice() {
 }
 
 
-function getHumanChoice() {
-    let humChoice = prompt();
-
+function getHumanChoice(selection) {
+    let humChoice = selection;
+   
     if(humChoice.toLowerCase() !== "rock" && humChoice.toLowerCase() !== "paper" &&  humChoice.toLowerCase() !== "scissor") {
         return 0;
     }
@@ -37,50 +44,56 @@ function getHumanChoice() {
 }
 
 
-function playRound(humChoice, compChoice) {
+function playRound(humChoice, compChoice, selection) {
 
-    let human = humChoice();
+    let human = humChoice(selection);
     let computer = compChoice();
     if(human == "rock" && computer == "Paper") {
         compScore += 1;
-        return "Computer Wins!!!!";
+        p.innerText = "Computer Wins!!!!";
     }
     
     else if(human == "rock" && computer == "scissors") {
         humScore += 1;
-        return "Player Wins!!";
+        p.innerText = "Player Wins!!";
     }
     else if(human == "paper" && computer == "rock") {
         humScore += 1;
-        return "Player Wins!!";
+        p.innerText = "Player Wins!!";
     }
     else if(human == "paper" && computer == "scissors") {
         compScore += 1;
-        return "Computer Wins!!";
+        p.innerText = "Computer Wins!!";
     }
     else if(human == "scissors" && computer == "rock") {
         compScore += 1;
-        return "Computer Wins";
+        p.innerText = "Computer Wins";
     }
     else if(human == "scissors" && computer == "paper") {
         humScore += 1;
-        return "Player Wins!!";
+        p.innerText = "Player Wins!!";
     }
     else {
-        return "Tie!!";
+        p.innerText = "Tie!!";
     }
+    score.textContent = "Player Score: " + humScore;
+    compSc.textContent = "Computer Score: " + compScore
 }
 
-function playGame() {
+// function playGame() {
     
-    for(let i = 0; i < 5; i++) {
-        let round = playRound(getHumanChoice, getComputerChoice);
-        console.log(round);
-    }
-
-    console.log(`Player won: ${humScore} games.` );
-    console.log(`Computer won: ${compScore} games.`);
     
-}
+//     let round = playRound(getHumanChoice, getComputerChoice);
+//     console.log(round);
+    
 
-playGame();
+//     console.log(`Player won: ${humScore} games.` );
+//     console.log(`Computer won: ${compScore} games.`);
+    
+// }
+
+
+
+rock.addEventListener('click', (e) => playRound(getHumanChoice, getComputerChoice, "rock"));
+paper.addEventListener('click', (e) => playRound(getHumanChoice, getComputerChoice, "paper"));
+scissors.addEventListener('click', (e) => playRound(getHumanChoice, getComputerChoice, "scissors"));
